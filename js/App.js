b732c9eca2a23a8e91f526579960cbd95c5cae8f
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-01-13 12:52:35 +0100

    initial commit

---
+++ js/App.js
@@ -0,0 +1,61 @@
+/**
+ *
+ * @param {*} openId
+ * @param {*} menuId
+ * @returns
+ */
+const showMenu = (openId, menuId) => {
+  const menu = document.querySelector(`.${menuId}`)
+  const openBtn = document.querySelector(`#${openId}`)
+  const mainMenu = document.querySelector('#menu')
+
+  if (openBtn && menu) {
+    openBtn.addEventListener("click", () => {
+      menu.classList.add("active");
+      mainMenu.classList.add('main-menu')
+    })
+  }
+
+  return (closeId) => {
+    const closeBtn = document.querySelector(`.${closeId}`)
+    closeBtn.addEventListener('click', event => {
+      event.target.parentElement.classList.remove('active')
+    })
+  }
+}
+
+/**
+ *
+ * @param {*} dropId
+ * @param {*} dropBtn
+ */
+const showDropDown = (dropId, dropBtn) => {
+  const menu = document.getElementById(`${dropId}`)
+  const dropButton = document.querySelector(`button span.${dropBtn}`)
+
+  dropButton.addEventListener('click', () => {
+    if (dropButton.classList.contains('fa-plus')) {
+      menu.classList.add('show')
+      dropButton.classList.replace('fa-plus', 'fa-minus')
+    } else {
+      menu.classList.remove('show')
+      dropButton.classList.replace('fa-minus', 'fa-plus')
+    }
+  })
+}
+
+(() => {
+  showMenu('open-btn', 'nav-menu-container')('close-icon')
+  showDropDown('dropMenu', 'drop-icon')
+})()
+
+
+const navLinks = document.querySelectorAll(".nav-item a.nav-link");
+function linkAction() {
+  navLinks.forEach((n) => n.classList.remove("active-link"));
+  this.classList.add("active-link");
+
+  const navMenu = document.querySelector(`.nav-menu-container`);
+  navMenu.classList.remove("show");
+}
+navLinks.forEach((m) => m.addEventListener("click", linkAction));
