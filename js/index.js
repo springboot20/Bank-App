a500fdb74305b8acdc52bc1cdb4b51fe88a53092
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-01-15 13:49:40 +0100

    Files modified

---
+++ js/index.js
@@ -0,0 +1,6 @@
+addEventListener('load', () => {
+  const creatbtn = document.querySelector('button.create')
+  creatbtn.addEventListener('click', () => {
+    window.location.href = './signup.html'
+  })
+})
