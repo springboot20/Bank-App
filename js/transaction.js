77039b4c469bb59a3ccdedd4b7b9a154d0daeee1
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-01-23 23:00:51 +0100

    app.html file updated by changing dropdouwn menu icon and app.css files modified base on dropdown menu icon

---
+++ js/transaction.js
@@ -0,0 +1,42 @@
+import showMenu from "./helper.js"
+
+/**
+ *
+ * @param {*} dropId
+ * @param {*} dropBtn
+ */
+
+const showDropDown = (dropId, dropBtn) => {
+	const menu = document.getElementById(`${dropId}`);
+	const dropB = document.querySelector(`span.${dropBtn}`);
+
+	dropB.addEventListener('click', () => {
+		if (dropB.classList.contains('fa-plus')) {
+			menu.classList.add('show');
+			dropB.classList.replace('fa-plus', 'fa-minus');
+		} else {
+			menu.classList.remove('show');
+			dropB.classList.replace('fa-minus', 'fa-plus');
+		}
+	});
+}
+
+(() => {
+	showMenu('open-btn', 'nav-menu-container')('close-icon');
+	showDropDown('dropMenu', 'drop-icon');
+})();
+
+const navLinks = document.querySelectorAll(".nav-item a.nav-link");
+function linkAction() {
+	navLinks.forEach((n) => n.classList.remove("active-link"));
+	this.classList.add("active-link");
+
+	const navMenu = document.querySelector(`.nav-menu-container`);
+	navMenu.classList.remove("show");
+}
+navLinks.forEach((m) => m.addEventListener("click", linkAction));
+
+
+addEventListener('load', () => {
+	document.body.classList.add('loaded');
+});
